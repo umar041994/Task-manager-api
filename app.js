78bfd8fb79fda8024300721f7efc5921f224791e
Omar Faruk <uf041994@gmail.com>
2026-07-06 20:38:24 +0600

    refactor mongoose connection

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 
 const dns = require('dns')
 dns.setServers(['8.8.8.8', '8.8.4.4']);
-require('./db/connection');
+
 
 const express = require('express');
 
 const app = express();
-const port =8000;  
+const port =process.env.PORT || 6000; 
+const connectDb = require('./db/connection');
+require('dotenv').config(); 
 
 
 
+const start = async () => {
+  try{
+    await connectDb(process.env.MONGOOSE_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    })    
+  }catch(error){
+      console.log('Error connecting to MongoDB:', error);
+    } 
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+start();
--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const dbURI = "mongodb+srv://rocky:12345@task-manager.j2jvtli.mongodb.net/?appName=TASK-MANAGER"
 
-mongoose.connect(dbURI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+
+
+ 
+
+  const connectDb = (url)=>{
+  mongoose.connect(url)
+  }
+
+module.exports = connectDb;
